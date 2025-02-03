159b3512f25bca8656f359dd8a1c87a9bf588133
Herman Ciechanowiec <herman.ciechanowiec@ds.pl>
2025-02-03 15:58:20 +0100

    DXP-1606: Introduce super type check

--- blueprints/src/main/java/dev/streamx/aem/connector/blueprints/FranklinCheck.java
+++ blueprints/src/main/java/dev/streamx/aem/connector/blueprints/FranklinCheck.java
@@ -1,11 +1,12 @@
 package dev.streamx.aem.connector.blueprints;
 
 import com.day.cq.wcm.api.Page;
+import java.util.Optional;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Optional;
 
 class FranklinCheck {
 
@@ -20,10 +21,22 @@ class FranklinCheck {
 
   boolean isFranklinPage() {
     boolean isFranklinPage = Optional.ofNullable(pageToCheck.getContentResource())
-        .map(Resource::getResourceType)
-        .map(resourceType -> resourceType.matches(FRANKLIN_RES_TYPE_REGEX))
+        .map(this::isFranklinResType)
         .orElse(false);
     LOG.trace("Is {} a Franklin page? Answer: {}", pageToCheck, isFranklinPage);
     return isFranklinPage;
+  }
+
+  private boolean isFranklinResType(Resource resource) {
+    ResourceResolver resourceResolver = resource.getResourceResolver();
+    String parentResourceType = Optional.ofNullable(
+        resourceResolver.getParentResourceType(resource)
+    ).orElse(StringUtils.EMPTY);
+    String resourceSuperType = Optional.ofNullable(resource.getResourceSuperType())
+        .orElse(StringUtils.EMPTY);
+    String resourceType = resource.getResourceType();
+    return parentResourceType.matches(FRANKLIN_RES_TYPE_REGEX)
+        || resourceSuperType.matches(FRANKLIN_RES_TYPE_REGEX)
+        || resourceType.matches(FRANKLIN_RES_TYPE_REGEX);
   }
 }
