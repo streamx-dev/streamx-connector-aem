f8f2a9f5e3a4be5323dd1e4b113c746689d3934a
Marek Czajkowski <76772327+marekczajkowski@users.noreply.github.com>
2025-04-22 14:14:06 +0200

    [DXP-1914] Revert "DXP-1743: Add support for Publish instance (#47)" (#50)
    This reverts commit 72bb497f

--- connector/src/test/java/dev/streamx/aem/connector/impl/AemReplicationEventHandlerTest.java
+++ connector/src/test/java/dev/streamx/aem/connector/impl/AemReplicationEventHandlerTest.java
@@ -7,54 +7,31 @@ import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.day.cq.commons.Externalizer;
 import com.day.cq.replication.ReplicationAction;
-import com.day.cq.replication.ReplicationActionType;
 import dev.streamx.sling.connector.StreamxPublicationException;
 import dev.streamx.sling.connector.StreamxPublicationService;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import org.apache.commons.lang3.math.NumberUtils;
-import org.apache.sling.discovery.DiscoveryService;
-import org.apache.sling.discovery.InstanceDescription;
-import org.apache.sling.discovery.TopologyView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.osgi.service.event.Event;
-import org.osgi.service.event.EventAdmin;
 
 @ExtendWith({MockitoExtension.class, AemContextExtension.class})
 class AemReplicationEventHandlerTest {
 
   private final AemContext context = new AemContext();
+  private AemReplicationEventHandler handler;
   private List<String> publishedPaths;
   private List<String> unpublishedPaths;
 
   @SuppressWarnings("ReturnOfNull")
   @BeforeEach
   void setup() throws StreamxPublicationException {
-    publishedPaths = List.of();
-    unpublishedPaths = List.of();
-    context.runMode(Externalizer.PUBLISH);
-    DiscoveryService discoveryService = new DiscoveryService() {
-      @Override
-      public TopologyView getTopology() {
-        TopologyView topologyView = mock(TopologyView.class);
-        InstanceDescription instanceDescription = mock(InstanceDescription.class);
-        when(topologyView.getLocalInstance()).thenReturn(instanceDescription);
-        when(instanceDescription.isLeader()).thenReturn(true);
-        return topologyView;
-      }
-    };
-    context.registerService(DiscoveryService.class, discoveryService);
     StreamxPublicationService streamxPublicationService = mock(StreamxPublicationService.class);
     doAnswer(
         invocation -> {
@@ -78,13 +55,11 @@ class AemReplicationEventHandlerTest {
     ).when(streamxPublicationService).unpublish(anyList());
     when(streamxPublicationService.isEnabled()).thenReturn(true);
     context.registerService(StreamxPublicationService.class, streamxPublicationService);
-    context.registerInjectActivateService(
-        AemReplicationEventHandler.class, Map.of("push-from", Externalizer.PUBLISH)
-    );
+    handler = context.registerInjectActivateService(AemReplicationEventHandler.class);
   }
 
   @Test
-  void consumeEvents() {
+  void test() {
     Event activate = new Event(
         ReplicationAction.EVENT_TOPIC,
         Map.of(
@@ -96,26 +71,19 @@ class AemReplicationEventHandlerTest {
             "userId", "admin"
         )
     );
-    Map<String, Object> props = new HashMap<>();
-    props.put(
-        "paths", new String[]{
-            "http://localhost:4502/content/we-retail/us/pl",
-            "/content/wknd/us/pl"
-        }
-    );
-    props.put("type", ReplicationActionType.DEACTIVATE);
-    Collection<Map<String, Object>> modifications = new ArrayList<>();
-    modifications.add(props);
     Event deactivate = new Event(
-        "com/adobe/granite/replication",
+        ReplicationAction.EVENT_TOPIC,
         Map.of(
-            "modifications", modifications,
+            "type", "Deactivate",
+            "paths", new String[]{
+                "http://localhost:4502/content/we-retail/us/pl",
+                "/content/wknd/us/pl"
+            },
             "userId", "admin"
         )
     );
-    EventAdmin eventAdmin = Objects.requireNonNull(context.getService(EventAdmin.class));
-    eventAdmin.sendEvent(activate);
-    eventAdmin.sendEvent(deactivate);
+    handler.handleEvent(activate);
+    handler.handleEvent(deactivate);
     assertAll(
         () -> assertEquals(
             List.of("http://localhost:4502/content/we-retail/us/en", "/content/wknd/us/en"),
