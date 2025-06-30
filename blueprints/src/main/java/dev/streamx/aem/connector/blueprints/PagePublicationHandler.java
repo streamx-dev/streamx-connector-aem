b09efe410269da1bcc49039c17bfc9f87359495b
Stanisław Ruchlicki <165791712+stan-r-ds@users.noreply.github.com>
2025-06-30 11:17:18 +0200

    DXP-1549 Unify publication handlers (#70)

--- blueprints/src/main/java/dev/streamx/aem/connector/blueprints/PagePublicationHandler.java
+++ blueprints/src/main/java/dev/streamx/aem/connector/blueprints/PagePublicationHandler.java
@@ -2,9 +2,7 @@ package dev.streamx.aem.connector.blueprints;
 
 import dev.streamx.blueprints.data.Page;
 import dev.streamx.sling.connector.PublicationHandler;
-import dev.streamx.sling.connector.PublishData;
 import dev.streamx.sling.connector.ResourceInfo;
-import dev.streamx.sling.connector.UnpublishData;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 import org.apache.sling.api.resource.Resource;
@@ -16,15 +14,11 @@ import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.propertytypes.ServiceDescription;
 import org.osgi.service.metatype.annotations.Designate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Component(service = PublicationHandler.class)
 @Designate(ocd = PagePublicationHandlerConfig.class)
 @ServiceDescription("Publication handler for AEM Pages")
 public class PagePublicationHandler extends BasePublicationHandler<Page> {
-
-  private static final Logger LOG = LoggerFactory.getLogger(PagePublicationHandler.class);
 
   private final PageDataService pageDataService;
   private final AtomicReference<PagePublicationHandlerConfig> config;
@@ -65,46 +59,28 @@ public class PagePublicationHandler extends BasePublicationHandler<Page> {
   }
 
   @Override
-  public PublishData<Page> getPublishData(String resourcePath) {
-    try (ResourceResolver resourceResolver = createResourceResolver()) {
-      Resource resource = resourceResolver.getResource(resourcePath);
-
-      if (resource == null) {
-        LOG.error("Resource not found when trying to publish it: {}", resourcePath);
-        return null;
-      }
-
-      Map<String, String> messageProps = getSxTypeAsMap(
-          resource,
-          config.get().rel_path_to_node_with_jcr_prop_for_sx_type(),
-          config.get().jcr_prop_name_for_sx_type()
-      );
-
-      return new PublishData<>(
-          getPagePath(resourcePath),
-          config.get().publication_channel(),
-          Page.class,
-          getPageModel(resource, resourceResolver),
-          messageProps
-      );
-    }
+  protected String getPublicationChannel() {
+    return config.get().publication_channel();
   }
 
   @Override
-  public UnpublishData<Page> getUnpublishData(String resourcePath) {
-    return new UnpublishData<>(
-        getPagePath(resourcePath),
-        config.get().publication_channel(),
-        Page.class);
-  }
-
-  private static String getPagePath(String resourcePath) {
-    return resourcePath + ".html";
-  }
-
-  private Page getPageModel(Resource resource, ResourceResolver resourceResolver) {
+  protected Page generateModel(Resource resource, ResourceResolver resourceResolver) {
     String content = pageDataService.getStorageData(resource, resourceResolver);
     return new Page(content);
+  }
+
+  @Override
+  protected Map<String, String> getMessageProps(Resource resource) {
+    return getSxTypeAsMap(
+        resource,
+        config.get().rel_path_to_node_with_jcr_prop_for_sx_type(),
+        config.get().jcr_prop_name_for_sx_type()
+    );
+  }
+
+  @Override
+  protected String getPublicationKey(String resourcePath) {
+    return resourcePath + ".html";
   }
 
 }
